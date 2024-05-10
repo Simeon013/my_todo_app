d32c8fe984fa0f7a843c2fd77b0386cf97ebc633
Simeon013 <karlsnow000@gmail.com>
2024-05-10 13:25:07 +0100

    Initialization Hive DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:my_todo_app/data/hive_data_store.dart';
+import 'package:my_todo_app/models/task.dart';
 import 'package:my_todo_app/views/home/home_view.dart';
-import 'package:my_todo_app/views/tasks/task_view.dart';
 
-Future<void> main() async{
-  runApp(const MyApp());
+Future<void> main() async {
+  // Initiative DB before run app
+  await Hive.initFlutter();
+
+  // Register adapter
+  Hive.registerAdapter<Task>(TaskAdapter());
+
+  // Open box
+  Box box = await Hive.openBox<Task>(HiveDataStore.boxName);
+
+  // Delete data from previous day
+  for (var task in box.values) {
+    if (task.createdAtTime.day != DateTime.now().day) {
+      task.delete();
+    } else {
+      // Do nothing
+    }
+  }
+
+  runApp(BaseWidget(child: const MyApp()));
+}
+
+class BaseWidget extends InheritedWidget {
+  BaseWidget({super.key, required this.child}) : super(child: child);
+  // BaseWidget(this.child, {super.key, required this.child}) : super(key: key, child: child);
+
+  final HiveDataStore dataStore = HiveDataStore();
+  final Widget child;
+
+  static BaseWidget of(BuildContext context) {
+    // return context.dependOnInheritedWidgetOfExactType<BaseWidget>()!;
+    final base = context.dependOnInheritedWidgetOfExactType<BaseWidget>();
+    if (base != null) {
+      return base;
+    } else {
+      throw StateError('No BaseWidget found in context');
+    }
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -17,45 +59,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Todo App',
       theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Colors.black,
-            fontSize: 45,
-            fontWeight: FontWeight.bold
-          ),
-          titleMedium: TextStyle(
-            color: Colors.grey,
-            fontSize: 16,
-            fontWeight: FontWeight.w300
-          ),
-          displayMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 21
-          ),
-          displaySmall: TextStyle(
-            color: Color.fromARGB(255, 234, 234, 234),
-            fontSize: 14,
-            fontWeight: FontWeight.w400
-          ),
-          headlineMedium: TextStyle(
-            color: Colors.grey,
-            fontSize: 17
-          ),
-          headlineSmall: TextStyle(
-            color: Colors.grey,
-            fontSize: 16
-          ),
-          titleSmall: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w500
-          ),
-          titleLarge: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w300,
-            fontSize: 40
-          )
-        )
-      ),
+          textTheme: const TextTheme(
+              displayLarge: TextStyle(
+                  color: Colors.black,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold),
+              titleMedium: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300),
+              displayMedium: TextStyle(color: Colors.white, fontSize: 21),
+              displaySmall: TextStyle(
+                  color: Color.fromARGB(255, 234, 234, 234),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400),
+              headlineMedium: TextStyle(color: Colors.grey, fontSize: 17),
+              headlineSmall: TextStyle(color: Colors.grey, fontSize: 16),
+              titleSmall:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+              titleLarge: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 40))),
       home: const HomeView(),
       // home: const TaskView(),
     );
@@ -70,7 +95,6 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
