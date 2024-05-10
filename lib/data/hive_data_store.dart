0d5c36ecff8f35925f621a7a05eb1df2f61bb5a0
Simeon013 <karlsnow000@gmail.com>
2024-05-10 13:01:05 +0100

    Create all crud operations

---
+++ lib/data/hive_data_store.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_todo_app/models/task.dart';
+
+// All [CRUD] operations
+class HiveDataStore {
+  // Box Name - String
+  static const boxName = 'taskBox';
+
+  //  Our current box with all the saved data inside - Box<Task>
+  final Box<Task> box = Hive.box<Task>(boxName);
+
+  // Add new task to box
+  Future addTask(Task task) async {
+    // await box.add(task);
+    await box.put(task.id, task);
+  }
+
+  // Show task
+  Future<Task?> getTask(String id) async {
+    return box.get(id);
+  }
+
+  // Update task
+  Future updateTask(Task task) async {
+    // await box.put(task.id, task);
+    await task.save();
+  }
+
+  // Delete task
+  Future deleteTask(Task task) async {
+    await task.delete();
+  }
+
+  // Listen for changes
+  ValueListenable<Box<Task>> listenToTasks() => box.listenable();
+}
---
+++ lib/models/task.dart
@@ -0,0 +1,48 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  Task({
+    required this.id,
+    required this.title,
+    required this.subtitle,
+    required this.createdAtTime,
+    required this.createdAtDate,
+    required this.isCompleted,
+    // required this.isCompleted,
+  });
+
+  @HiveField(0) // Unique ID
+  final String id;
+  @HiveField(1) // Title
+  String title;
+  @HiveField(2) // Subtitle
+  String subtitle;
+  @HiveField(3) // Created At Time
+  DateTime createdAtTime;
+  @HiveField(4) // Created At Date
+  DateTime createdAtDate;
+  @HiveField(5) // Is Completed
+  bool isCompleted;
+
+  // Create a new task
+  factory Task.create({
+    required String? title,
+    required String? subtitle,
+    DateTime? createdAtTime,
+    DateTime? createdAtDate,
+  }) {
+    return Task(
+      id: const Uuid().v1(),
+      title: title ?? '',
+      subtitle: subtitle ?? '',
+      createdAtTime: createdAtTime ?? DateTime.now(),
+      createdAtDate: createdAtDate ?? DateTime.now(),
+      isCompleted: false,
+    );
+  }
+
+}
